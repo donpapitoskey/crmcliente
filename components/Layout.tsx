f1d9320bc3f3916dea406a0f265dc8030e64f600
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2020-10-30 15:23:55 -0500

    form management with formik

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,7 +17,7 @@ const Layout = ({ children}: Props) => {
   <>
     <Head>
       <meta charSet="utf-8" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossOrigin="anonymous" />
       <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
--- pages/newaccount.tsx
+++ pages/newaccount.tsx
@@ -1,12 +1,86 @@
 import React from "react";
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
+import {useFormik} from "formik";
 
-const NewAccount = () => (
+const NewAccount = () => {
+  
+  const formik = useFormik({
+    initialValues: {
+      nombre: 'Name',
+      apellido: '',
+      email: '',
+      password: '',
+    },
+    onSubmit: valores => {
+      console.log('enviando');
+      console.log(valores);
+    }
+  });
+  
+  return(
   <>
     <Layout>
-    <h1>New account</h1>
+      <h1 className="text-center text-2xl text-white font-white">Setup new account</h1>
+      <div className="flex justify-center mt-5">
+        <div className="w-full max-w-sm">
+          <form 
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+            onSubmit={formik.handleSubmit}
+          > 
+          <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                  Nombre
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="nombre"
+                  type="text"
+                  placeholder="Nombre Usuario"
+                  value={formik.values.nombre}
+                  onChange={formik.handleChange}/>
+            </div>
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                  Apellido
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="apellido"
+                  type="text"
+                  placeholder="Apellido Usuario"
+                  value={formik.values.apellido}
+                  onChange={formik.handleChange}/>
+            </div>
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                  Email
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Email Usuario"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}/>
+            </div>
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                  Password
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Email Usuario"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}/>
+            </div>
+            <input 
+              type="submit"
+              className="bg-gray-800 w-full mt-*5 p-2 text-white uppercase hover:bg-gray-900"
+              value="Creat Cuenta"
+            />
+          </form>
+        </div>
+      </div>
     </Layout>
   </>
-);
+)};
 
 export default NewAccount;
