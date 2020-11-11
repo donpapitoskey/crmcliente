559db8ecb9e8474c124559d55166ccf28fca0f90
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2020-11-10 21:38:37 -0500

    Token passing through localstorage sucessful

--- config/apollo.ts
+++ config/apollo.ts
@@ -1,13 +1,31 @@
-import { ApolloClient, HttpLink, InMemoryCache, InMemoryCacheConfig} from '@apollo/client';
+import {
+  ApolloClient,
+  HttpLink,
+  ApolloLink,
+  InMemoryCache,
+  concat,
+} from '@apollo/client';
 import fetch from 'cross-fetch';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/',
+  fetch,
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('token');
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  });
+  return forward(operation);
+});
 
 const client = new ApolloClient({
   connectToDevTools: true,
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:4000/',
-    fetch,
-  })
+  link: concat(authMiddleware, httpLink),
 });
 
 export default client;
